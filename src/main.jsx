475cd011939b79878528387ff9954a9fe2a3a2f4
LuisAcevedo777 <leavleav01@gmail.com>
2024-09-10 18:32:42 -0500

    panthera updated4

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import '../styles/App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootswatch/dist/lux/bootstrap.min.css';
 
